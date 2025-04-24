29491d53514357eeddd13c9d53a366e430e20da0
Deepstud <2928855@hc.ac.je>
2025-04-24 11:50:58 +0100

    Adding a new game page
    Currently working on the tailwind

---
+++ app/routes/add-game.tsx
@@ -0,0 +1,11 @@
+export default function AddGame() {
+  return (
+    <div className="container mx-auto py-20 ">
+      {
+        <h1 className="font-bold text-size=lg text-centre">
+          Add <span className="text-blue">game</span>
+        </h1>
+      }
+    </div>
+  );
+}
