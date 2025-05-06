71fbc63f8f350ce4d4b282d97691735ac5a048b9
Deepstud <2928855@hc.ac.je>
2025-05-06 15:19:51 +0100

    Moving game information into a component called game card

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,6 +5,7 @@ import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import type { MetaFunction } from "@remix-run/node";
 
+import GameCard from "~/components/game-card";
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -26,34 +27,7 @@ export default function Index() {
   console.log({ games });
   return (
     <div className="flex flex-wrap justify-evenly my-auto py-4">
-      {games.map((game) => (
-        <div
-          key={game.id}
-          className="w-auto h-auto flex flex-col items-center justify-between p-2  "
-        >
-          <section>
-            <img
-              className="h-48 w-full object-cover rounded"
-              src="/app/assets/png/field-game-background.png"
-              alt={game.title}
-            />
-          </section>
-          <section className=" flex flex-col">
-            <h2 className="text-center font-semibold text-wrap">
-              {game.title}
-            </h2>
-            <h3 className="text-center text-gray-700">£{game.price}</h3>
-          </section>
-          <section className="  flex-col ml-10">
-            <button className="flex border-2 border-cyan-100 rounded-sm my-2">
-              Edit
-            </button>
-            <button className=" flex border-2 border-red-600 rounded-sm my-2">
-              Delete
-            </button>
-          </section>
-        </div>
-      ))}
+      {games.map((game) => GameCard(game))}
     </div>
   );
 }
