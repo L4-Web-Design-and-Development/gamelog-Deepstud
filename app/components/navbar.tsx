37128c9bcf8102c3ab7891ebf0468b73725198eb
Deepstud <2928855@hc.ac.je>
2025-05-06 15:36:13 +0100

    Fixing the Nav bar's alignment

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,22 +2,22 @@ import siteLogo from "~/assets/svg/gamelog-logo.svg";
 import { Link } from "@remix-run/react";
 export default function NavBar() {
   return (
-    <nav className="container mx-auto flex items-start py-8 justify-between">
-      <div className="container mx-auto flex items-center">
+    <nav className="w-full flex items-start justify-between py-8 px-8">
+      <section className="flex items-start">
         <Link to="/">
-          <img className="h-10 w-auto mr-4" src={siteLogo} alt="logo " />
+          <img className="h-10 w-auto mr-4" src={siteLogo} alt="logo" />
         </Link>
         <h1 className="text-5xl font-bold">
           Game <span className="text-blue-800">Log</span>!
         </h1>
-      </div>
-      <div className="container flex gap-8 mx-auto justify-end text-lg">
+      </section>
+      <section className="flex gap-8 text-center text-lg justify-end">
         <Link to="/add-games" className="whitespace-nowrap">
           Add Games
         </Link>
         <Link to="/blog">Blog</Link>
         <Link to="/about">About</Link>
-      </div>
+      </section>
     </nav>
   );
 }
